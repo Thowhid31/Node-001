789b799dff9a72461f1ae3f8fa70cda52bf7c96f
Thowhid31 <thowhidantor16@gmail.com>
2022-06-03 00:01:53 +0600

    implement users

--- index.js
+++ index.js
@@ -6,8 +6,42 @@ app.get('/', (req, res)=>{
     res.send('Hello, I can code with node.js')
 })
 
+const users = [
+    {
+        id: 1,
+        name: 'Shabana',
+        email: 'sabana@gmail.com',
+        phone: '01703040'
+    },
+    {
+        id: 2,
+        name: 'Shana',
+        email: 'shana@gmail.com',
+        phone: '01703041'
+    },
+    {
+        id: 3,
+        name: 'Shaina',
+        email: 'shaina@gmail.com',
+        phone: '01703042'
+    },
+    {
+        id: 4,
+        name: 'Subah',
+        email: 'subah@gmail.com',
+        phone: '01703043'
+    }
+]
+
 app.get('/users', (req, res)=>{
-    res.send('Hello from users')
+    res.send(users)
+})
+
+app.get('/user/:id', (req, res)=> {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id)
+    res.send(user)
 })
 
 app.listen(port, () => {
