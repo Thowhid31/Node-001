7dd317fb84a92c92095bd38cb4a1c3e0b38d5ed7
Thowhid31 <thowhidantor16@gmail.com>
2022-06-02 23:35:15 +0600

    added update for port

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000
+
+app.get('/', (req, res)=>{
+    res.send('Hello, I can code with node.js')
+})
+
+app.listen(port, () => {
+    console.log('Listening to Practice Port');
+})
