8869e37a78e0985a308d1616e050aee999ef77ba
Thowhid31 <thowhidantor16@gmail.com>
2022-06-03 20:51:41 +0600

    uses of query

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
-app.use(cors())
+app.use(cors());
+app.use(express.json())
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res)=>{
@@ -36,7 +37,14 @@ const users = [
 ]
 
 app.get('/users', (req, res)=>{
+    if(req.query.name){
+    const search = req.query.name.toLowerCase();
+    const matched = users.filter(user => user.name.toLowerCase().includes(search) )
+    res.send(matched)
+    }else{
     res.send(users)
+    }
+    
 })
 
 app.get('/user/:id', (req, res)=> {
@@ -48,7 +56,10 @@ app.get('/user/:id', (req, res)=> {
 
 app.post('/user', (req, res)=>{
     console.log('request', req.body);
-    res.send('Post Succeed')
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user)
+    res.send(user)
 })
 
 app.listen(port, () => {
